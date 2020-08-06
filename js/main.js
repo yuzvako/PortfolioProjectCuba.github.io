dc676931029dd7001765b74dbf53c88d49527eeb
yuzvako <69280497+yuzvako@users.noreply.github.com>
2020-08-06 02:27:03 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+
+    $('.photo__slider').slick({
+       prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-l.svg" alt=""></button>',
+       nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-r.svg" alt=""></button>' 
+    });
+
+});
